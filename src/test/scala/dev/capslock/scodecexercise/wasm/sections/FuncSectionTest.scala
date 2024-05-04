c8fe438c1cdfca1c4e0d2acb1c5188cf42048b60
windymelt <windymelt@3qe.us>
2024-05-04 12:57:57 +0900

    fixed broken tests

--- src/test/scala/dev/capslock/scodecexercise/wasm/sections/FuncSectionTest.scala
+++ src/test/scala/dev/capslock/scodecexercise/wasm/sections/FuncSectionTest.scala
@@ -7,14 +7,15 @@ class FuncSectionTest extends UnitTest:
   describe("FuncSection") {
     it("should encode") {
       val funcSection = FuncSection(Vector(0))
-      val result = FuncSection.codecWithSize(1).encode(funcSection).require
+      val result =
+        FuncSection.codecWithSize(2).encode(funcSection).require.bytes
       // magic number is omitted because this class is for just payload
       result shouldBe hex"01 00"
     }
     it("should decode") {
       val bytes =
         hex"01 00" // Function Section(code 03), size 2, count 1, index 0
-      val result = FuncSection.codecWithSize(1).decodeValue(bytes.bits).require
+      val result = FuncSection.codecWithSize(2).decodeValue(bytes.bits).require
       result shouldBe FuncSection(Vector(0))
     }
   }
