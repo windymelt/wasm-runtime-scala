aa5e2ee8878b38a510d4279c17d98479206d1746
windymelt <windymelt@3qe.us>
2024-05-06 01:54:56 +0900

    refactoring

--- src/test/scala/dev/capslock/scodecexercise/wasm/package.scala
+++ src/test/scala/dev/capslock/scodecexercise/wasm/package.scala
@@ -2,9 +2,9 @@ package dev.capslock.scodecexercise
 
 package object wasm:
   def wat2wasm(wat: String): Array[Byte] = {
-    val tmpFile = os.temp(wat, prefix = "wat2wasm", deleteOnExit = true)
+    val watFile = os.temp(wat, prefix = "wat2wasm", deleteOnExit = true)
     val process = os
-      .proc("/usr/local/bin/wat2wasm", "--output=-", tmpFile.toString)
+      .proc("/usr/local/bin/wat2wasm", "--output=-", watFile.toString)
       .spawn()
     process.stdout.readAllBytes()
   }
